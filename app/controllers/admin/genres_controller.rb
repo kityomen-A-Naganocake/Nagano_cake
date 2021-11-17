1bb39975d034354828b0ef9e50e2b73985729be2
asatokoki <okometabero@me.com>
2021-11-17 11:48:12 +0000

    ディベロップあげる前ー

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,15 @@
 class Admin::GenresController < ApplicationController
+
+    def index
+    end
+    
+    def edit
+    end
+    
+    def create
+    end
+    
+    def update
+    end
+
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,22 @@
 class Public::OrdersController < ApplicationController
+
+    def index
+    end
+    
+    def show
+    end
+    
+    def new
+    end
+    
+    def confirm
+    end
+    
+    def complete
+    end
+    
+    def create
+    end
+
+
 end
