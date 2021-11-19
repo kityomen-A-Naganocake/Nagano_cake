439c6074662dd3f69f395b7aaff849f2a9affd81
yamato090 <tomoki.y.0924@icloud.com>
2021-11-19 03:18:03 +0000

    add admin showページ　完了

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,15 +1,22 @@
 class Admin::CustomersController < ApplicationController
 
     def index
+        @customers = Customer.all
     end
     
     def show
+        @customer = Customer.find(params[:id])
     end
     
     def edit
+        @customer = Customer.find(params[:id])
     end
     
     def update
     end
     
+    private
+	def customer_params
+	  params.require(:customer).permit(:first_name,:last_name,:kana_first_name,:kana_last_name,:postal_code,:residence,:phone_number,:email,:is_valid)
+	end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
           validates :email, presence: true
           validates :last_name, presence: true
           validates :first_name, presence: true
@@ -12,5 +12,13 @@ class Customer < ApplicationRecord
           validates :zip_code, presence: true
           validates :address, presence: true
           validates :telephone_number, presence: true
+          # 会員フルネーム
+          def full_name
+            self.first_name + " " + self.last_name
+          end
+          # 会員カナフルネーム
+          def kana_full_name
+            self.first_name_kana + " " + self.last_name_kana
+          end
 
 end
