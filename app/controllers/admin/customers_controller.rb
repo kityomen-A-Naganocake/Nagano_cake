5df58388c8661706fc0003292904f810941b5064
yamato090 <tomoki.y.0924@icloud.com>
2021-11-20 04:38:36 +0000

    add admin 退会ステータス　作成途中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -16,6 +16,7 @@ class Admin::CustomersController < ApplicationController
     def update
       @customer = Customer.find(params[:id])
       if @customer.update(customer_params)
+        
         redirect_to admin_customer_path
       else
         render :edit
--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,7 +2,22 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  
+  
+    # 退会処理機能
+    protected
+    # 退会しているかを判断するメソッド
+    def customer_state
+      ## 【処理内容1】 入力されたemailからアカウントを1件取得
+      @customer = Customer.find_by(email: parms[:customer][:email])
+      ## アカウントを取得できなかった場合、このメソッドを終了する
+      return if !@customer
+      ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+      if @customer.valid_password?(params[:customer][:password])
+        ## 【処理内容3】
+      end
+    end
+   
   # GET /resource/sign_in
   # def new
   #   super
