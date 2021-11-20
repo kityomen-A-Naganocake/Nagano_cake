f87526c634c16521b038a3be6f26e11d2febd826
mixteitei <jittoshiteruyo@gmail.com>
2021-11-20 07:50:24 +0000

    admin_item_is_activeバリデーション変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
     attachment :image
-    validates :name, :price_no_tax, :genre_id, :is_active, :description, :image, presence:true
+    validates :name, :price_no_tax, :genre_id, :description, :image, presence:true
+    validates :is_active, inclusion:  {in: [true, false]}
+    belongs_to :genre
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5505536fb8a9fdff6a731ebfb8b3c507ca7789e466c0e89139db20d5d28648e1c438955b2e75b8f2c940f239b5ee84875fe243c534fdb2961de8f2c3d77ba22c'
