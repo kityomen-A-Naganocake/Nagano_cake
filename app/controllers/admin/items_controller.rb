217166b81e59e093c4eee8c77b88ecfbbf251d18
yamato090 <yamauchi09012@gmail.com>
2021-11-21 08:09:45 +0000

    add 注文にいくため

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ItemsController < ApplicationController
 
     def index
-        @admin_items = Item.all.page(params[:page]).per(8)
+        @admin_items = Item.all
     end
 
     def new
