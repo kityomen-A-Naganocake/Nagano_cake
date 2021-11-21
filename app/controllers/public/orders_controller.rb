afe9c08986b60a4d6ee8f91c2c02cd857a50ef92
yamato090 <yamauchi09012@gmail.com>
2021-11-21 12:24:08 +0000

    add order 作成途中_1

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,13 +10,36 @@ class Public::OrdersController < ApplicationController
     end
     
     def confirm
+        @order = Order.find(params[:id])
+        @order_details = @order.order_details
     end
     
     def complete
     end
     
     def create
+    @item = Item.find(params[:item_id])
+    @order = @item.order.new(order_params)
+    @order.save
+    redirect_to items_path
     end
-
-
+    
+    
+    private
+    
+    def order_params
+        params.require(:order).permit(:customer_id,:address,:item_id,:payment_amount,:shipping_cost,:status)
+    end
+    # もしかしたらshipping_addresses_paramsかも
+    def address_params
+        params.require(:order).permit(:customer_id,:last_name,:first_name,:zip_code,:address)
+    end
+    # 商品合計（税込）の計算
+    #def calculate(user)
+     #   total_price = 0
+     #   user.cart_items.each do |cart_item|
+     #      payment_amount += cart_item.quantity * cart_item.item.price
+     #   end
+     #   return (total_price * 1.1).floor
+    #end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,17 @@
 class Order < ApplicationRecord
-    enum payment_method: { credit_card:0, transfer:1 }
+    # enum payment_method: { credit_card:0, transfer:1 }
+    
+    belongs_to :customer
+	  has_many   :order_details, dependent: :destroy
+	  enum order_status: [:入金待ち, :入金確認, :製作中, :発送準備中, :発送済み]
+  	enum payment: [:クレジットカード, :銀行振込]
+    
+    # ステータス変更
+    #after_update do
+    #if self.status == "入金確認"
+    #  self.order_details.each {|order_detail|
+    #  order_detail.update(item_status: "製作待ち")
+    #  }
+    #end
+  #end
 end
